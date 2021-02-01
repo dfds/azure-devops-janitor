f5e2b7c279d8e8dfcf84e1d47e28ba2659fcd453
Tobias Andersen <toban@dfds.com>
2021-02-01 15:19:23 +0100

    - Aligned ReleaseDefinitionDto with documentation

--- src/AzureDevOpsJanitor.Infrastructure/Vsts/DataTransferObjects/ReleaseDefinitionDto.cs
+++ src/AzureDevOpsJanitor.Infrastructure/Vsts/DataTransferObjects/ReleaseDefinitionDto.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace AzureDevOpsJanitor.Infrastructure.Vsts.DataTransferObjects
 {
@@ -10,7 +11,19 @@ namespace AzureDevOpsJanitor.Infrastructure.Vsts.DataTransferObjects
         [JsonPropertyName("name")]
         public string Name { get; set; }
 
-        [JsonPropertyName("url")]
-        public string Url { get; set; }
+        [JsonPropertyName("path")]
+        public int Path { get; set; }
+
+        [JsonPropertyName("projectReference")]
+        public ProjectDto Project { get; set; }
+
+        [JsonPropertyName("tags")]
+        public IEnumerable<string> Tags { get; set; }
+
+        [JsonPropertyName("artifacts")]
+        public IEnumerable<ArtifactDto> Artifacts { get; set; }
+
+        [JsonPropertyName("environments")]
+        public IEnumerable<EnvironmentDto> Environments { get; set; }
     }
 }
