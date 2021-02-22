94cae2cdd15f7a84057f22c41821c29e2228ae78
Tobias Andersen <toban@dfds.com>
2021-02-22 14:51:17 +0100

    - removed event forwarder (moving to own solution)

--- src/AzureDevOpsJanitor.Host.EventForwarder/Enablers/ApiKey/IApiKeyService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace AzureDevOpsJanitor.Host.EventForwarder.Enablers.ApiKey
-{
-    public interface IApiKeyService
-    {
-        Task<bool> IsAuthorized(string clientId, string apiKey);
-    }
-}
